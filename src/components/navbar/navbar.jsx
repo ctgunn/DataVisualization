c314a6411ae7c4fde833985340cc47d1d3ed28c3
Cliff Gunn <cliff.gunn@gmail.com>
2017-01-05 00:20:21 -0600

    navbar.jsx doesn't work or have anything in it really. Just created it real quick as a place holder and because I want to see if my contributions are working for github.com now.

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './css/app.css';
+
+class Navbar extends Component {
+    render() {
+        return (
+
+        );
+    }
+}
+
+export default Navbar;
